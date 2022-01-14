e243c5572433c4f2ed9ad50f69e5e3b619641ec0
Ryan Stewart <P22rstewart@fairfieldprep.org>
2022-01-13 19:05:51 -0500

    fixed

--- lab_2-1.py
+++ lab_2-1.py
@@ -1,11 +1,11 @@
 # Author RTS 1/10/22
+
 def double_stuff(lst):
     for i, v in enumerate(lst):
         lst[i] = 2 * v
     return lst
 
 
-print(double_stuff([0, 1, 3, 4]) == [2, 4, 6, 8])
-print(double_stuff([1.1, 3, 4.1]) == [2.2, 4, 6.1, 8])
-print(double_stuff(["A", 1, 5, 8]))
-lst = input("Enter a list: ")
+print(double_stuff([2, 3, 4]) == [4, 6, 8])
+print(double_stuff([2, 2.5, 3.5]) == [4, 5, 7])
+print(double_stuff([2, 2.5, "two"]) == [4, 5, "twotwo"])
---
+++ lab_3-1.py
@@ -0,0 +1,21 @@
+# Author RTS 1/10/22
+
+def add_foods(lst):
+    sixth_letter = []
+    not_foods = []
+    short_foods = []
+    for x in lst:
+        try:
+            sixth_letter.append(x[5])
+        except TypeError:
+            not_foods.append(x)
+        except IndexError:
+            short_foods.append(x)
+
+    print("sixth_letter: ", sixth_letter)
+    print("not_foods: ", not_foods)
+    print("short_foods: ", short_foods)
+
+add_foods(['potatoes', 'salsa', 'cherries', 'banana', 'apple'])
+add_foods(['naan', 'celery', 'buckwheat', 7, 'clementine'])
+add_foods(['cheesburger', True, 'chicken', 'rice', 'radish'])
